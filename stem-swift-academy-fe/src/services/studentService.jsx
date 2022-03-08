232c37ea1fccac7684f945ad6d2fd2564d9c8822
Alex <alexandrina.mehandzhiyska1@gmail.com>
2022-03-08 18:28:47 +0200

    debug requests

--- stem-swift-academy-fe/src/services/studentService.jsx
+++ stem-swift-academy-fe/src/services/studentService.jsx
@@ -1,6 +1,8 @@
 const getExams = async (studentId) => {
   const response = await fetch(`/students/${studentId}`);
   const data = await response.json();
+  const text = await response.text();
+  console.log(text);
 
   if (!response.ok) {
     throw new Error(data);
