a964eb0011f2fd0d0747d75a0b40713e5addbe9a
Alex <alexandrina.mehandzhiyska1@gmail.com>
2022-03-22 20:31:38 +0200

    debug update functionality

--- src/services/examService.js
+++ src/services/examService.js
@@ -4,11 +4,11 @@ const getAll = (subject) => db.query('SELECT * FROM exams WHERE subject = $1', [
 const getOne = (id) => db.query('SELECT * FROM exams WHERE id = $1', [id]);
 
 const createOne = async(subject, section, instructions, duration, difficulty, link, text) => {
-    const exam = await db.query('INSERT INTO exams (subject, section, duration, instructions, text, link) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [subject, section, duration, instructions, text, link]);
+    const exam = await db.query('INSERT INTO exams (subject, section, duration, instructions, text, link, difficulty) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [subject, section, duration, instructions, text, link, difficulty]);
     return exam.rows[0];
 }
 
-const updateOne = async(examId, subject, section, instructions, difficulty, duration, text, link) => {
+const updateOne = async(examId, subject, section, instructions, duration, difficulty, text, link) => {
     const exam = await db.query('UPDATE exams SET subject = $1, section = $2, duration = $3, instructions = $4, text = $5, link = $6, difficulty = $7 WHERE id = $8 RETURNING *', [subject, section, duration, instructions, text, link, difficulty, examId]);
     return exam.rows[0];
 }
