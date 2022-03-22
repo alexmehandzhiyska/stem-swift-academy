52078c348b136a45b134b3b41345b82e94d35897
Alex <alexandrina.mehandzhiyska1@gmail.com>
2022-03-22 14:31:40 +0200

    add questions info on results page

--- stem-swift-academy-fe/src/components/Exams/ExamResults/QuestionResult.jsx
+++ stem-swift-academy-fe/src/components/Exams/ExamResults/QuestionResult.jsx
@@ -1,11 +1,11 @@
 import './ExamResults.css'
 
 const QuestionResult = ({ question, userAnswer }) => {
-    return (
+    return (   
         <article className="my-16 ml-36">
             <h1 className="my-6 question-title">{question.title}</h1>
             <p>{question.choices.map(answer => 
-                <li className={answer == question.correct_answer ? "text-green-400" : "text-black"}>{answer}</li>
+                <li className={answer == question.correct_answer ? "text-green-400" : answer == userAnswer ? "text-red-500" : "text-black"}>{answer}</li>
             )}</p>
         </article>
     );
