f3017247a73519b7b0b7d05d5ac0d08d775c75d5
Alex <alexandrina.mehandzhiyska1@gmail.com>
2022-03-22 18:49:55 +0200

    Hide text field for math exams

--- stem-swift-academy-fe/src/components/Exams/CreateExam/CreateExam.jsx
+++ stem-swift-academy-fe/src/components/Exams/CreateExam/CreateExam.jsx
@@ -10,39 +10,44 @@ import LottieAnimation from '../../LottieAnimation';
 const CreateExam = () => {
   const [exam, setExam] = useState(null);
   const [questions, setQuestions] = useState([]);
+  const [examSubject, setExamSubject] = useState('english');
   const [charsLeft, setCharsLeft] = useState(2500);
   const [isLoading, setIsLoading] = useState(true);
 
   const questionsArr = Array(10).fill(1);
   const { register, formState: { errors }, handleSubmit } = useForm({ mode: 'onSubmit', reValidateMode: 'onChange' });
-  const { subject, examId } = useParams();
+  let { subject, examId } = useParams();
+
   const mode = examId ? 'edit' : 'create'
   const navigate = useNavigate();
 
   useEffect(() => {
     if (mode === 'edit') {
-      examService.getOne(subject, examId)
+      examService.getOne(examSubject, examId)
         .then(response => setExam(response.data.exam))
         .catch(() => errorNotification('There was an error loading the exam data. Please try again later!'));
 
-      examService.getQuestions(subject, examId, false)
+      examService.getQuestions(examSubject, examId, false)
         .then(response => {
           setQuestions(response.data.questions)
           setIsLoading(false);
         })
         .catch(() => errorNotification('There was an error loading the exam data. Please try again later!'));
+
+      setExamSubject(subject);
+      console.log(examSubject);
     } else {
       setIsLoading(false);
     }
-  }, [subject, examId, mode]);
+  }, [examSubject, examId, mode]);
 
 
   const submitExamHandler = (data) => {
     if (mode === 'edit') {
-      examService.updateOne(subject, examId, data)
+      examService.updateOne(examSubject, examId, data)
         .then(response => {
           successNotification('Exam edited successfully!');
-          navigate(`/exams/${subject}/${examId}`);
+          navigate(`/exams/${examSubject}/${examId}`);
         })
         .catch(error => {
           errorNotification('There was an error editing your exam. Please try again later.');
@@ -68,6 +73,7 @@ const CreateExam = () => {
     setCharsLeft(2500 - chars);
   }
 
+
   return (
     <>
       {isLoading && <LottieAnimation />}
@@ -76,25 +82,25 @@ const CreateExam = () => {
           <h1 className="heading capitalize">{mode} Test</h1>
 
           <form className="subject-select flex flex-col items-center" onSubmit={handleSubmit(submitExamHandler)}>
-            <select className="capitalize" defaultValue={mode === 'edit' ? exam.subject : 'english'} {...register('subject')} name="subject">
+            <select className="capitalize" {...register('subject')} defaultValue={mode === 'edit' ? exam.subject : 'english'}  onChange={e => setExamSubject(e.target.value)} name="subject">
               <option value="english">english</option>
               <option value="math">math</option>
             </select>
 
             <fieldset className="non-english-inputs flex flex-col items-center">
               <input name="section" {...register('section', { required: { value: true, message: 'Field is required!' } })} className="exam-input" placeholder="Section" type="text" defaultValue={mode === 'edit' ? exam.section : ''} />
-              {errors.section && <p>{errors.section.message}</p>}
+              {errors.section && <p className="text-blue-500">{errors.section.message}</p>}
               <input name="instructions" {...register('instructions', { required: { value: true, message: 'Field is required!' } })} className="exam-input" placeholder="Instructions" type="text" defaultValue={mode === 'edit' ? exam.instructions : ''} />
-              {errors.instructions && <p>{errors.instructions.message}</p>}
+              {errors.instructions && <p className="text-blue-500">{errors.instructions.message}</p>}
               <input name="duration" {...register('duration', { required: { value: true, message: 'Field is required!' } })} className="exam-input" placeholder="Test duration (in minutes)" type="number" defaultValue={mode === 'edit' ? exam.duration : ''} />
-              {errors.duration && <p>{errors.duration.message}</p>}
+              {errors.duration && <p className="text-blue-500">{errors.duration.message}</p>}
               <input name="difficulty" {...register('difficulty', { required: { value: true, message: 'Field is required!' } })} className="exam-input" placeholder="Test difficulty (Low, Medium or High)" type="text" defaultValue={mode === 'edit' ? exam.difficulty : ''} />
-              {errors.difficulty && <p>{errors.difficulty.message}</p>}
+              {errors.difficulty && <p className="text-blue-500">{errors.difficulty.message}</p>}
               <input name="pdfLink" {...register('pdfLink')} className="exam-input" placeholder="PDF link" type="text" defaultValue={mode === 'edit' ? exam.link : ''} />
             </fieldset>
 
             <article className="english-inputs flex flex-col items-center">
-              <textarea name="text" {...register('text', { maxLength: { value: 2500, message: 'Text cannot be longer than 2500 characters!' } })} onChange={charactersChange} className={`exam-input`} placeholder="Text" type="text" defaultValue={mode === 'edit' ? exam.text : ''}></textarea>
+              <textarea name="text" {...register('text', { maxLength: { value: 5000, message: 'Text cannot be longer than 5000 characters!' } })} onChange={charactersChange} className={examSubject == 'english' ? 'exam-input' : 'hidden'} placeholder="Text" type="text" defaultValue={mode === 'edit' ? exam.text : ''}></textarea>
 
               {/* <h3 className="chars-left">Characters left: {charsLeft}</h3> */}
 
@@ -105,15 +111,15 @@ const CreateExam = () => {
                 <h2 className="question-header heading">Question {i + 1}</h2>
 
                 <input {...register(`questions.question-${i + 1}.title`, { required: { value: true, message: 'Field is required!' } })} className="exam-input" type="text" placeholder="Question" name={`questions.question-${i + 1}.title`} defaultValue={mode === 'edit' ? questions[i].title : ''} />
-                {errors[`questions.question-${i + 1}.title`] && <p>{errors[`questions.question-${i + 1}.title`].message}</p>}
+                {errors[`questions.question-${i + 1}.title`] && <p className="text-blue-500">{errors[`questions.question-${i + 1}.title`].message}</p>}
                 <input {...register(`questions.question-${i + 1}.correctAnswer`, { required: { value: true, message: 'Field is required!' } })} className="exam-input" type="text" placeholder="Correct Answer" name={`questions.question-${i + 1}.correctAnswer`} defaultValue={mode === 'edit' ? questions[i].correct_answer : ''} />
-                {errors[`questions.question-${i + 1}.correctAnswer`] && <p>{errors[`questions.question-${i + 1}.correctAnswer`].message}</p>}
+                {errors[`questions.question-${i + 1}.correctAnswer`] && <p className="text-blue-500">{errors[`questions.question-${i + 1}.correctAnswer`].message}</p>}
                 <input {...register(`questions.question-${i + 1}.wrongAnswer1`, { required: { value: true, message: 'Field is required!' } })} className="exam-input" type="text" placeholder="First wrong answer" name={`questions.question-${i + 1}.wrongAnswer1`} defaultValue={mode === 'edit' ? questions[i].choices[1] : ''} />
-                {errors[`questions.question-${i + 1}.wrongAnswer1`] && <p>{errors[`questions.question-${i + 1}.wrongAnswer1`].message}</p>}
+                {errors[`questions.question-${i + 1}.wrongAnswer1`] && <p className="text-blue-500">{errors[`questions.question-${i + 1}.wrongAnswer1`].message}</p>}
                 <input {...register(`questions.question-${i + 1}.wrongAnswer2`, { required: { value: true, message: 'Field is required!' } })} className="exam-input" type="text" placeholder="Second wrong answer" name={`questions.question-${i + 1}.wrongAnswer2`} defaultValue={mode === 'edit' ? questions[i].choices[2] : ''} />
-                {errors[`questions.question-${i + 1}.wrongAnswer2`] && <p>{errors[`questions.question-${i + 1}.wrongAnswer2`].message}</p>}
+                {errors[`questions.question-${i + 1}.wrongAnswer2`] && <p className="text-blue-500">{errors[`questions.question-${i + 1}.wrongAnswer2`].message}</p>}
                 <input {...register(`questions.question-${i + 1}.wrongAnswer3`, { required: { value: true, message: 'Field is required!' } })} className="exam-input" type="text" placeholder="Third wrong answer" name={`questions.question-${i + 1}.wrongAnswer3`} defaultValue={mode === 'edit' ? questions[i].choices[3] : ''} />
-                {errors[`questions.question-${i + 1}.wrongAnswer3`] && <p>{errors[`questions.question-${i + 1}.wrongAnswer3`].message}</p>}
+                {errors[`questions.question-${i + 1}.wrongAnswer3`] && <p className="text-blue-500">{errors[`questions.question-${i + 1}.wrongAnswer3`].message}</p>}
 
               </article>
             )}
