4aec473715164f9cb7237cdcdf84d43c38ab77fa
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2022-04-26 23:01:20 +0300

    Fixed bug on radio selection

--- stem-swift-academy-fe/src/components/Exams/ExamQuestions/ExamQuestions.jsx
+++ stem-swift-academy-fe/src/components/Exams/ExamQuestions/ExamQuestions.jsx
@@ -84,13 +84,13 @@ const ExamQuestions = () => {
           <form className={text ? "survey" : "survey min-w-full"} onSubmit={handleSubmit(submitAnswers)}>
             {questions.length == 0 ? <p>This exam still has no questions</p> : questions.map((question, i) => 
               <article key={i} className="my-10 mx-20">
-                <p className="font-bold text-3xl mb-6">{i + 1}. {question.title}</p>
+                <p className="font-bold text-2xl mb-6">{i + 1}. {question.title}</p>
 
                 {question.image_url && <img src={question.image_url}></img>}
                 {question.choices.map((choice, i1) => 
                   <section key={i1} className="my-2 text-xl">
-                    <input type="radio" id={choice} value={choice} {...register(`${i + 1}`)} name={`${i + 1}`}/>
-                    <label htmlFor={choice} className="ml-5">{choice}</label>
+                    <input type="radio" id={`question${i + 1}${choice}`} value={choice} {...register(`${i + 1}`)} name={`${i + 1}`}/>
+                    <label htmlFor={`question${i + 1}${choice}`} className="ml-5">{choice}</label>
                   </section>
                 )}
               </article>
